63e55ba240aa6a62b656cd379f0a54ab71325fb2
RossLabCSU <49657843+RossLabCSU@users.noreply.github.com>
2023-12-12 10:44:55 -0700

    Add files via upload

---
+++ analyze_GOresults.py
@@ -0,0 +1,93 @@
+
+import os
+from tqdm import tqdm
+domains = ['Archaea', 'Bacteria', 'Eukaryota', 'Viruses']     
+amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
+all_lcd_classes = list(amino_acids)
+for res1 in amino_acids:
+    for res2 in amino_acids.replace(res1, ''):
+        all_lcd_classes.append( res1+res2 )
+        
+cwd = os.getcwd()
+
+def main():
+
+    df = initialize_df()
+    org_count = {'Archaea':294,
+                 'Bacteria':7533,
+                 'Eukaryota':2061,
+                 'Viruses':6134}
+
+    for domain in domains:
+        dirname = os.path.join(cwd, domain + '_GOresults')
+        
+        for file in tqdm(os.listdir(dirname)):
+            if not file.endswith('RESULTS.tsv'):
+                continue
+                
+            file_info = file.split('_')
+            org_id = file_info[0] + '_' + file_info[1]
+
+            lcd_class = file_info[2].replace('class', '')
+                
+            h = open(os.path.join(dirname, file))
+            header = h.readline()
+            
+            for line in h:
+                items = line.rstrip().split('\t')
+                go_id = items[0]
+                go_category = items[1]
+                enr_or_pur = items[2]
+                go_desc = items[3].strip()
+                hits_in_pop = int(items[7])
+                totalprots = int(items[8])
+                depth = int(items[12])
+                perc_hits_in_total = str(round(hits_in_pop/totalprots*100, 3))
+                if enr_or_pur != 'e' or depth < 4:
+                    continue
+                p_sidak = float(items[15])
+                go_info = '\t'.join([go_id, go_category, go_desc]) 
+                if p_sidak < 0.05:
+                    df[domain][lcd_class][go_info] = df[domain][lcd_class].get(go_info, [])
+                    df[domain][lcd_class][go_info].append( (p_sidak, org_id, perc_hits_in_total) )
+
+            h.close()
+
+    output_data(df, org_count, domain)
+    
+
+def output_data(df, org_count, domain):
+
+    output = open('TableS6_GOterm_Summary_AllOrganisms_AllClasses.tsv', 'w')
+    output.write('\t'.join(['Domain of Life', 'LCD Class', 'GO term ID', 'GO term Category', 'GO term Description', 'Number of Organisms with GO term Significantly Enriched', 'Total Number of Organisms Analyzed', 'Percentage of Organisms with GO term Significantly Enriched', 'Sidak-corrected p-values (semicolon-delimited)', 'Corresponding Organism UniProt IDs (semicolon-delimited)', 'Percentage of GO term-Associated Proteins in Background Proteome']) + '\n')
+    for domain in domains:
+        total_orgs = org_count[domain]
+        for lcd_class in all_lcd_classes:
+            for go_info in df[domain][lcd_class]:
+                data = df[domain][lcd_class][go_info]
+                data = sorted(data)
+                pvals = [str(tup[0]) for tup in data]
+                org_ids = ';'.join([tup[1] for tup in data])
+                perc_hits = ';'.join([tup[2] for tup in data])
+                num_sig = len(pvals)
+                pvals = ';'.join(pvals)
+                output.write('\t'.join([domain, lcd_class, go_info, str(num_sig), str(total_orgs), str(num_sig/total_orgs*100), pvals, org_ids, perc_hits]) + '\n')
+    output.close()
+
+        
+def initialize_df():
+    df = {}
+    for domain in domains:
+        df[domain] = {}
+        for res1 in amino_acids:
+            for res2 in amino_acids:
+                if res1 == res2:
+                    df[domain][res1] = {}
+                else:
+                    df[domain][res1+res2] = {}
+                    
+    return df
+    
+
+if __name__ == '__main__':
+    main()
---
+++ plot_TopRanking_GOterms_DomainsOfLife.py
@@ -0,0 +1,150 @@
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from matplotlib.patches import Patch
+
+domains = ['Archaea', 'Bacteria', 'Eukaryota', 'Viruses']
+fig_labels = ['Fig2C', 'Fig2D', 'Fig2B', 'FigS9']
+
+def main():
+
+    plotting_category = 'SecondaryLCDs'
+
+    master_df = {'Domain':[],
+                'Percentage of Organisms\nwith Significant Enrichment':[]}
+    
+    for domain_of_interest in domains:
+        if domain_of_interest == 'Eukaryota':
+            num_terms_plotting = 50
+        else:
+            num_terms_plotting = 25
+
+        h = open('TableS6_GOterm_Summary_AllOrganisms_AllClasses.tsv')
+
+        goterms = []
+        perc_orgs = []
+        lcd_classes = []
+        go_categories = []
+
+        header = h.readline()
+        
+        for line in h:
+            items = line.rstrip().split('\t')
+            
+            domain = items[0]
+            if domain != domain_of_interest:
+                continue
+                
+            lcd_class = items[1]
+            if len(lcd_class) == 1 and plotting_category == 'SecondaryLCDs':
+                continue
+            elif len(lcd_class) == 2 and plotting_category == 'PrimaryLCDs':
+                continue
+                
+            perc_enr = float(items[7])
+            
+            goterms.append(items[4])
+            go_categories.append(items[3])
+            perc_orgs.append(perc_enr)
+            lcd_classes.append(lcd_class)
+            
+        h.close()
+    
+        perc_orgs, goterms, lcd_classes, go_categories = zip(*sorted(zip(perc_orgs, goterms, lcd_classes, go_categories), reverse=True))
+
+        master_df['Domain'] += [domain_of_interest for x in range(len(perc_orgs))]
+        master_df['Percentage of Organisms\nwith Significant Enrichment'] += perc_orgs
+        goterms = np.array(goterms)
+        perc_orgs = np.array(perc_orgs)
+        
+        plotting(domain_of_interest, goterms, go_categories, perc_orgs, lcd_classes, plotting_category, num_terms_plotting)
+        
+    plot_stripplot(master_df)
+    
+        
+def plot_stripplot(master_df):
+
+    sns.stripplot(x='Domain', y='Percentage of Organisms\nwith Significant Enrichment', data=master_df, alpha=0.2, size=3, jitter=0.4)
+    plt.xticks(fontname='Arial', fontsize=12, rotation=90)
+    plt.yticks(fontname='Arial', fontsize=12)
+    plt.xlabel('Domain of Life', fontname='Arial', fontsize=14)
+    plt.ylabel('Percentage of Organisms\nwith Significant Enrichment', fontname='Arial', fontsize=14)
+    fig = plt.gcf()
+    fig.set_size_inches(2, 6)
+    plt.savefig('Fig2A_All_LCDclasses_PercentOrganismsWithSigEnrichment_Stripplot.tif', bbox_inches='tight', dpi=600, pil_kwargs={'compression':'tiff_lzw'})
+    plt.close()
+    
+        
+def plotting(domain, goterms, go_categories, perc_orgs, lcd_classes, plotting_category, num_terms_plotting):
+    
+    tick_colors = ['#be0119', '#00035b', '#0b5509'] # SCARLET, DARK BLUE, FOREST
+    cats = ['BP', 'MF', 'CC']
+    tick_cmap = {cat:tick_colors[i] for i, cat in enumerate(cats)}
+    
+    colors = list(sns.color_palette(n_colors=50))
+    
+    xtick_labels = goterms[:num_terms_plotting]
+    perc_orgs = perc_orgs[:num_terms_plotting]
+    lcd_classes = lcd_classes[:num_terms_plotting]
+
+    encountered_lcd_classes = set()
+    nonredundant_lcd_classes = []
+    for lcd_class in lcd_classes:
+        if lcd_class not in encountered_lcd_classes:
+            reverse_class = lcd_class[1] + lcd_class[0]
+            if reverse_class in nonredundant_lcd_classes:
+                loc = nonredundant_lcd_classes.index(reverse_class)
+                new_class = reverse_class + '/' + lcd_class
+                nonredundant_lcd_classes[loc] = new_class
+            else:
+                nonredundant_lcd_classes.append(lcd_class)
+        encountered_lcd_classes.add(lcd_class)
+
+    color_map = {}
+    color_map_nonredundant = {}
+    for i, lcd_class in enumerate(nonredundant_lcd_classes):
+        color_map_nonredundant[lcd_class] = colors[i]
+        classes = lcd_class.split('/')
+        for c in classes:
+            color_map[c] = colors[i]
+
+    df = {'GO term':xtick_labels,
+            'Percentage of Organisms\nwith Significant Enrichment':perc_orgs,
+            'Color':[color_map[lcd_class] for lcd_class in lcd_classes]}
+    plt.bar([x for x in range(len(xtick_labels))], perc_orgs[:num_terms_plotting], color=[color_map[lcd_class] for lcd_class in lcd_classes])
+    for i, val in enumerate(perc_orgs):
+        offset = max(perc_orgs) * 0.01
+        plt.text(i, val+offset, lcd_classes[i], ha='center', fontname='Arial', fontsize=14)
+
+    legend_elements = [Patch(facecolor=color_map_nonredundant[lcd_class], label=lcd_class) for lcd_class in color_map_nonredundant]
+                         
+    legend = plt.legend(handles=legend_elements, handletextpad=0.3, ncol=2, title='LCD Class', prop={'family':'Arial', 'size':14})
+    plt.setp(legend.get_title(), fontsize=15, fontname='Arial')
+    
+    plt.xticks([x for x in range(len(xtick_labels))], labels=xtick_labels, rotation=90, va='top', fontname='Arial', fontsize=14)
+    plt.yticks(fontname='Arial', fontsize=14)
+    
+    ax = plt.gca()
+    xticklabels = ax.get_xticklabels()
+    for i, label in enumerate(go_categories[:num_terms_plotting]):
+        xticklabels[i].set_color(tick_cmap[label])
+        
+    plt.xlabel('GO term', fontname='Arial', fontsize=16)
+    plt.ylabel('Percentage of Organisms\nwith Significant Enrichment', fontname='Arial', fontsize=16)
+
+    fig = plt.gcf()
+    if num_terms_plotting == 25:
+        fig.set_size_inches(10, 6.5)
+    else:
+        fig.set_size_inches(20, 6.5)
+        
+    plt.xlim(-1, num_terms_plotting)
+    index = domains.index(domain)
+    fig_label = fig_labels[index]
+    plt.savefig(fig_label + '_' + domain + '_Top' + str(num_terms_plotting) + '_GOterms_' + plotting_category + '.tif', bbox_inches='tight', dpi=600, pil_kwargs={'compression':'tiff_lzw'})
+    plt.close()
+    
+        
+if __name__ == '__main__':
+    main()
